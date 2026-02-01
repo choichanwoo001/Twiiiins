edd2ed57ea0871d9925173c664a4a4441591c79b
choichanwoo001 <114642968+choichanwoo001@users.noreply.github.com>
2026-02-01 20:49:06 +0900

    미사용 컴포넌트 삭제 (BaseInput, BaseForm, BaseModal)

--- frontend/src/components/common/index.js
+++ frontend/src/components/common/index.js
@@ -1,6 +1,4 @@
 // 공통 컴포넌트들을 export
 export { default as BaseButton } from './BaseButton.vue'
-export { default as BaseModal } from './BaseModal.vue'
-export { default as BaseInput } from './BaseInput.vue'
 export { default as ConfirmDialog } from './ConfirmDialog.vue'
 export { default as AlertDialog } from './AlertDialog.vue'
